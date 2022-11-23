a152f389c3d3e364dfed1594743b710ea1aeefdd
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-23 11:25:00 +0100

    Fixed main function, and also unused imports

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
@@ -11,5 +11,7 @@ async fn main() {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
 
-    let _ = run(listener).expect("Failed to run server");
+    run(listener)?.await?;
+
+    Ok(())
 }
