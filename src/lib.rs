7426682cc5c5dc74f6f282872bfd96cd65eb1764
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-23 12:22:23 +0100

    Parsing from data from a POST Request

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+/// We were returning `impl Responder` at the very beginning. We are now spelling out the type explicitly
+/// given that we have become more familiar with `actix-web`.
+/// There is no performance difference! Just a stylistic choice :)
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+        })
         .listen(listener)?
         .run();
 
