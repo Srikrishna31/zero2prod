791c4c551c4eea860b94e2ee7431dd6778425f3e
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-24 08:52:27 +0100

    Init DB script working in Linux

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -44,7 +44,9 @@ done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}"
 
-DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
-export DATABASE_URL
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 
 sqlx database create
+sqlx migrate run
+
+>& echo "Postgres has been migrated, ready to go!"
