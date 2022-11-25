a0138b0de9a1db61c99f5987163ec412dc85f22a
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-25 17:12:26 +0100

    Reduce the imports to last but one layer, as per Rust conventions

--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,4 @@
-use crate::routes::{health_check, subscribe};
+use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
@@ -12,8 +12,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
             // Register the connection as part of the application state
             .app_data(db_pool.clone())
     })
