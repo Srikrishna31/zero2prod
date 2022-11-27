4fe054caafdfcff605963dd7af81f97db94045eb
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-27 12:05:26 +0100

    Fix the cargo tests

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -14,7 +14,8 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
     // We are falling back to printing all spans at info-level or above if the RUST_LOG environment
     // variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
 
     let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
 
