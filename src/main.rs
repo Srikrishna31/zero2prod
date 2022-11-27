4a6e43543cf4f31efabfa702782931107ee74d9d
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-27 09:33:59 +0100

    Refactor logging configuration into telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use zero2prod::{configuration, startup};
+use zero2prod::{configuration, startup, telemetry};
 
 /// # tracing-subscriber
 /// `tracing-subscriber` does much more than providing us with a few handy subscribers. It introduces
@@ -27,30 +23,8 @@ use zero2prod::{configuration, startup};
 /// what spans should be processed, formatting span data, shipping span data to remote systems, etc.
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    //Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    // We are falling back to printing all spans at info-level or above if the RUST_LOG environment
-    // variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        //Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-
-    // The `with` method is provided by `SubscriberExt`, an extension trait for `Subscriber` exposed
-    // by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    // `set_global_default` can be used by applications to specify what subscriber should be used to
-    // process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
-
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
+    telemetry::init_subscriber(subscriber);
     //Panic if we can't read configuration
     let configuration = configuration::get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
@@ -70,3 +44,5 @@ async fn main() -> std::io::Result<()> {
 
     Ok(())
 }
+
+
