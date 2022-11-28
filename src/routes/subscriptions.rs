baf57823b3b010ef761180c0ccc4a8b64f019d2d
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-28 09:08:02 +0100

    Use tracing::instrument procedural macro to cleanup logging code

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono;
 use sqlx::{types::uuid, PgPool};
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -19,27 +18,52 @@ pub struct FormData {
 ///
 /// It is an interesting technique to perform what in other language ecosystems might be referred to
 /// as *dependency injection*.
+///
+/// # Cleaning up Instrumentation Code - tracing::Instrument
+///
+/// We'd like all operations within subscribe to happen within the context of request_span. In other
+/// words, we'd like to *wrap* the `subscribe` function in a span. `#[tracing::instrument]` creates
+/// a span at the beginning of the function invocation and automatically attaches all arguments passed
+/// to the function to the context of the span - in our case, `form` and `pool`. Often function arguments
+/// won't be displayable on log records (e.g. `pool`) or we'd like to specify more explicitly what
+/// should/how they should be captured (e.g. naming each field of `form`) - we can explicitly tell
+/// `tracing` to ignore them using the skip directive.
+///
+/// `name` can be used to specify the message associated to the function span - if omitted, it defaults
+/// to the function name.
+///
+/// We can also enrich the span's context using the `fields` directive. It leverages the same syntax
+/// we have already for the info_span! macro.
+///
+/// The result is quite nice: all instrumentation concerns are visually separated by execution
+/// concerns - the first are dealt with in a procedural macro that "decorates" the function declaration,
+/// while the function body focuses on the actual business logic.
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     // Retrieving a connection from the application state!
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    // Spans, like logs, have an associated level. `info_span` creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
-
-    // We don't call `.enter` on query_span! `.instrument` takes care of it at the right moments in
-    // the query future lifetime.
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -49,18 +73,13 @@ pub async fn subscribe(
         form.name,
         chrono::Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection` wrapped by `web::Data`.
-    .execute(pool.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            tracing::info!("request_id {request_id} - New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("request_id {request_id} - Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+        // Using the `?` operator to return early if the function failed, returning a sqlx::Error
+    })?;
+
+    Ok(())
 }
