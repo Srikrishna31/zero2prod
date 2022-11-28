71fde402dc65115ceb8b0348fc9464ee7a026e10
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-28 15:52:10 +0100

    Add production and local configuration for host address

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::{configuration, startup, telemetry};
 
@@ -28,16 +28,20 @@ async fn main() -> std::io::Result<()> {
     telemetry::init_subscriber(subscriber);
     //Panic if we can't read configuration
     let configuration = configuration::get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
     // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(&address).expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
 
-    println!("Running the server on: http://127.0.0.1:{port}");
+    println!("Running the server on: {address}: {port}");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
 
