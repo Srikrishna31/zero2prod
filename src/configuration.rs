028f2302b5a86c507528314b3d9c89895dc7964e
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-29 10:39:36 +0100

    Add Database connection information to Digital Ocean Deploymment spec

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,8 +1,9 @@
 use config::ConfigError;
-use sqlx::postgres::PgConnectOptions;
 use secrecy::{ExposeSecret, Secret};
 use serde;
 use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -27,6 +28,7 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
@@ -63,15 +65,24 @@ pub fn get_configuration() -> Result<Settings, ConfigError> {
 
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        options
     }
 
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
-            .password(&self.password.expose_secret())
+            .password(self.password.expose_secret())
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 }
 
