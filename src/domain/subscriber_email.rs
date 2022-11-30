26e914494eac4e27edc1b54bc6a4700b26f120f4
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-30 13:15:13 +0100

    Add SubscriberEmail structure and its relevant unit tests

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -19,10 +19,18 @@ impl AsRef<str> for SubscriberEmail {
     }
 }
 
+/// # Property-based Testing
+/// Property-based testing significantly increases the range of inputs that we are validating, and
+/// therefore our confidence in the correctness of our code, but it does not *prove* that our parser
+/// is correct - it does not *exhaustively* explore the input space (except for tiny ones).
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
     use claim::assert_err;
+    /// We are importing the `SafeEmail` faker! We also need the `Fake` trait to get access to the
+    /// `.fake` method on `SafeEmail`
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -40,5 +48,11 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        claim::assert_ok!(SubscriberEmail::parse(email));
     }
 }
