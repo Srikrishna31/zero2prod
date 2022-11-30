d46e2da76724f8c8e2942a497b35d348bf19e47f
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-30 12:29:21 +0100

    Add email validation function and the corresponding SubscriberEmail

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -1,5 +1,13 @@
 use crate::domain::subscriber_name::SubscriberName;
 
+/// # Type Driven Development
+/// Making an incorrect usage pattern unrepresentable, by construction is known as *type driven
+/// development*. It is a powerful approach to encode the constraints of a domain we are trying to
+/// model inside the type system, leaning on the compiler to make sure they are enforced.
+///
+/// The more expressive the type system of our programming language is, the tighter we can constrain
+/// our code to only be able to represent states that are valid in the domain we are working in. This
+/// particular pattern here is known as "new-type pattern" in the Rust community.
 pub struct NewSubscriber {
     pub email: String,
     pub name: SubscriberName,
