973b232d3a790e40256220d52902fdff69295c3b
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-01 10:54:11 +0100

    Make the tests green after adding email client

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,21 +1,29 @@
 use crate::domain::SubscriberEmail;
 use reqwest::{Client, Url};
+use secrecy::{ExposeSecret, Secret};
 
 pub struct EmailClient {
     http_client: Client,
-    base_url: reqwest::Url,
+    base_url: Url,
     sender: SubscriberEmail,
+    // We don't want to log this by accident
+    authorization_token: Secret<String>,
 }
 
 impl EmailClient {
-    pub fn new(base_url: String, sender: SubscriberEmail) -> Result<Self, String> {
-        match Url::parse(&base_url)  {
-            Ok(url) => Ok(        Self {
+    pub fn new(
+        base_url: String,
+        sender: SubscriberEmail,
+        authorization_token: Secret<String>,
+    ) -> Result<Self, String> {
+        match Url::parse(&base_url) {
+            Ok(url) => Ok(Self {
                 http_client: Client::new(),
                 base_url: url,
                 sender,
+                authorization_token,
             }),
-            Err(e) => Err(e.to_string())
+            Err(e) => Err(e.to_string()),
         }
     }
 
@@ -26,8 +34,7 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        let url = self.base_url.join("/email")
-            .map_err(|e| e.to_string())?;
+        let url = self.base_url.join("/email").map_err(|e| e.to_string())?;
 
         let request_body = SendEmailRequest {
             from: self.sender.as_ref().to_owned(),
@@ -37,13 +44,24 @@ impl EmailClient {
             text_body: text_content.to_owned(),
         };
 
-        let builder = self.http_client.post(url).json(&request_body);
+        let _builder = self
+            .http_client
+            .post(url)
+            .header(
+                "X-Postmark-Server-Token",
+                self.authorization_token.expose_secret(),
+            )
+            .json(&request_body)
+            .send()
+            .await
+            .map_err(|e| e.to_string())?;
+
         Ok(())
     }
 }
 
 #[derive(serde::Serialize)]
-struct SendEmailRequest{
+struct SendEmailRequest {
     from: String,
     to: String,
     subject: String,
@@ -51,11 +69,9 @@ struct SendEmailRequest{
     text_body: String,
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::domain::SubscriberEmail;
-    use crate::email_client::EmailClient;
+    use super::*;
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
@@ -66,7 +82,8 @@ mod tests {
         // Arrange
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client = EmailClient::new(mock_server.uri(), sender).unwrap();
+        let email_client =
+            EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake())).unwrap();
 
         Mock::given(any())
             .respond_with(ResponseTemplate::new(200))
@@ -76,10 +93,11 @@ mod tests {
 
         let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let subject: String = Sentence(1..2).fake();
-        let content: String = Paragraph(1 .. 10).fake();
+        let content: String = Paragraph(1..10).fake();
 
         // Act
-        let _ = email_client.send_email(subscriber_email, &subject, &content, &content)
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
             .await;
 
         // Assert
