7afb2ba113c98dc183f4165ff98d429b8ca6fb16
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-01 12:26:37 +0100

    Refactoring: Avoid unnecessary memory allocations

--- src/email_client.rs
+++ src/email_client.rs
@@ -37,11 +37,11 @@ impl EmailClient {
         let url = self.base_url.join("/email").map_err(|e| e.to_string())?;
 
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
 
         let _builder = self
@@ -62,12 +62,12 @@ impl EmailClient {
 
 #[derive(serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 #[cfg(test)]
