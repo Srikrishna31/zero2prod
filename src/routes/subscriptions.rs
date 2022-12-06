cf1c9b2346d805f3db961efba4abf989f41e0913
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-06 17:21:58 +0100

    Add anyhow::Error and more context to the errors to get the logs as before

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use crate::email_client::EmailClient;
 use crate::startup::ApplicationBaseUrl;
 use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
+use anyhow::Context as anyhow_ctx;
 use chrono;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
@@ -65,8 +66,8 @@ pub enum SubscribeError {
     /// do not implement the `Sized` marker trait. A `Box` stores the trait object itself on the heap,
     /// while we store the pointer to its heap location in `SubscribeError::UnexpectedError` the
     /// pointer itself has a known size at compile-time - problem solved, we are `Sized` again.
-    #[error("{1}")]
-    UnexpectedError(#[source] Box<dyn std::error::Error>, String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
 }
 
 impl std::fmt::Debug for SubscribeError {
@@ -79,7 +80,7 @@ impl ResponseError for SubscribeError {
     fn status_code(&self) -> StatusCode {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::UnexpectedError(_, _) => StatusCode::INTERNAL_SERVER_ERROR,
+            SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
@@ -192,39 +193,25 @@ pub async fn subscribe(
 ) -> Result<HttpResponse, SubscribeError> {
     // We no longer have `#[from]` for `ValidationError`, so we need to map the error explicitly.
     let new_subscriber = form.0.try_into().map_err(SubscribeError::ValidationError)?;
-    let mut transaction = pool.begin().await.map_err(|e| {
-        SubscribeError::UnexpectedError(
-            Box::new(e),
-            "Failed to acquire a Postgres connection from the pool".into(),
-        )
-    })?;
+    let mut transaction = pool
+        .begin()
+        .await
+        .context("Failed to acquire a Postgres connection from the pool")?;
     let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(
-                Box::new(e),
-                "Failed to insert new subscriber in the database.".into(),
-            )
-        })?;
+        .context("Failed to insert new subscriber in the database.")?;
     let subscription_token = generate_subscription_token();
 
     // The `?` operator transparently invokes the `Into` trait on our behalf - we don't need an
     // explicit `map_err` anymore.
     store_token(&mut transaction, subscriber_id, &subscription_token)
         .await
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(
-                Box::new(e),
-                "Failed to store the confirmation token for a new subscriber".into(),
-            )
-        })?;
+        .context("Failed to store the confirmation token for a new subscriber.")?;
 
-    transaction.commit().await.map_err(|e| {
-        SubscribeError::UnexpectedError(
-            Box::new(e),
-            "Failed to commit SQL transaction to store a new subscriber.".into(),
-        )
-    })?;
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit SQL transaction to store a new subscriber.")?;
 
     send_confirmation_email(
         &email_client,
@@ -234,9 +221,7 @@ pub async fn subscribe(
         &templates,
     )
     .await
-    .map_err(|e| {
-        SubscribeError::UnexpectedError(Box::new(e), "Failed to send a confirmation mail.".into())
-    })?;
+    .context("Failed to send a confirmation mail.")?;
 
     Ok(HttpResponse::Ok().finish())
 }
@@ -287,31 +272,28 @@ async fn send_confirmation_email(
     template_context.insert("confirmation_link", &confirmation_link);
     let html_body = templates
         .render("confirmation.html", &template_context)
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(
-                Box::new(e),
-                "Error rendering html email template.".into(),
-            )
-        })?;
+        .context("Error rendering html email template.")?;
 
     let plain_body = templates
         .render("confirmation.txt", &template_context)
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(
-                Box::new(e),
-                "Error rendering plain text email template.".into(),
-            )
-        })?;
+        .context("Error rendering plain text email template.")?;
 
     // We are ignoring email delivery errors for now.
     email_client
         .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e), "Error sending email".into()))?;
+        .context("Error sending email")?;
 
     Ok(())
 }
 
+/// As a rule of thumb: **Errors should be logged when they are handled.**
+///
+/// If your function is propagating the error upstream (e.g. using the ? operator), it should **not**
+/// log the error. It can, if it makes sense, add more context to it.
+///
+/// If the error is propagated all the way up to the request handler, delegate logging to a dedicated
+/// middleware - `tracing_actix_web::TracingLogger` in our case.
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
     skip(new_subscriber, transaction)
@@ -332,12 +314,8 @@ async fn insert_subscriber(
         chrono::Utc::now()
     )
     .execute(transaction)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-        // Using the `?` operator to return early if the function failed, returning a sqlx::Error
-    })?;
+    // Using the `?` operator to return early if the function failed, returning a sqlx::Error
+    .await?;
 
     Ok(subscriber_id)
 }
