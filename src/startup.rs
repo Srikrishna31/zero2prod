4a36d8ddf0d2d93f49c8045f966deab82bec7a4e
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-06 22:39:35 +0100

    A dummy implementation to make the test green

--- src/startup.rs
+++ src/startup.rs
@@ -128,6 +128,7 @@ fn run(
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .route("/subscriptions/confirm", web::get().to(routes::confirm))
+            .route("/newsletters", web::post().to(routes::publish_newsletter))
             // Register the connection as part of the application state
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
