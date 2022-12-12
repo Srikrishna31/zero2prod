f66417527db9c6509fabc99f4eb1a9cadf8e3a8e
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-12 08:10:19 +0100

    Change password field to password hash in the database

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,4 +1,5 @@
 use once_cell::sync::Lazy;
+use sha3::Digest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
@@ -10,6 +11,7 @@ pub(crate) struct TestApp {
     pub(crate) db_pool: PgPool,
     pub(crate) email_server: MockServer,
     pub(crate) port: u16,
+    pub(crate) test_user: TestUser,
 }
 
 /// Confirmation links embedded in the request to the email API.
@@ -71,12 +73,12 @@ impl TestApp {
     }
 
     pub async fn test_user(&self) -> (String, String) {
-        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1",)
+        let row = sqlx::query!("SELECT username, password_hash FROM users LIMIT 1",)
             .fetch_one(&self.db_pool)
             .await
             .expect("Failed to create test users.");
 
-        (row.username, row.password)
+        (row.username, row.password_hash)
     }
 }
 
@@ -139,9 +141,10 @@ pub(crate) async fn spawn_app() -> TestApp {
         db_pool: startup::get_connection_pool(&configuration.database),
         email_server,
         port,
+        test_user: TestUser::generate(),
     };
 
-    add_test_user(&test_app.db_pool).await;
+    test_app.test_user.store(&test_app.db_pool).await;
     test_app
 }
 
@@ -192,15 +195,34 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
     connection_pool
 }
 
-async fn add_test_user(pool: &PgPool) {
-    sqlx::query!(
-        "INSERT INTO users (user_id, username, password)
-        VALUES ($1, $2, $3)",
-        Uuid::new_v4(),
-        Uuid::new_v4().to_string(),
-        Uuid::new_v4().to_string(),
-    )
-    .execute(pool)
-    .await
-    .expect("Failed to create test users.");
+pub(crate) struct TestUser {
+    pub(crate) user_id: Uuid,
+    pub(crate) username: String,
+    pub(crate) password: String,
+}
+
+impl TestUser {
+    pub fn generate() -> Self {
+        Self {
+            user_id: Uuid::new_v4(),
+            username: Uuid::new_v4().to_string(),
+            password: Uuid::new_v4().to_string(),
+        }
+    }
+
+    async fn store(&self, pool: &PgPool) {
+        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
+        let password_hash = format!("{password_hash:x}");
+
+        sqlx::query!(
+            "INSERT INTO users (user_id, username, password_hash)\
+            VALUES ($1, $2, $3)",
+            self.user_id,
+            self.username,
+            password_hash,
+        )
+        .execute(pool)
+        .await
+        .expect("Failed to store test user.");
+    }
 }
