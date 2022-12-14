6443f5cfe2dfe482c4ccdb47ede53a99913a9ad1
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-14 12:03:53 +0100

    Add proper authentication to login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,11 @@
-use actix_web::http::header::LOCATION;
-use actix_web::{web, HttpResponse};
+use crate::authentication;
+use crate::authentication::{AuthError, Credentials};
+use crate::routes::error_chain_fmt;
+use actix_web::http::{header::LOCATION, StatusCode};
+use actix_web::{web, HttpResponse, ResponseError};
 use secrecy::Secret;
+use sqlx::PgPool;
+use std::fmt::Formatter;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -17,9 +22,54 @@ pub struct FormData {
 /// All redirect status codes are in the 3xx range -  we need to choose the most appropriate one
 /// depending on the HTTP verb and the semantic meaning we want to communicate(e.g. temporary vs
 /// permanent redirection).
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
-        //Go back to the home page.
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+
+    let user_id = authentication::validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::AuthError(e.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+    Ok(HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
-        .finish()
+        .finish())
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+        }
+    }
 }
