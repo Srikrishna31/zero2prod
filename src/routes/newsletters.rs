75da5507678d89f8d66cdeebb8e60bc1bbb71215
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-14 11:13:28 +0100

    Move authentication methods from newsletters into authentication module

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,15 +1,14 @@
-use crate::authentication::AuthError;
+use crate::authentication;
+use crate::authentication::{AuthError, Credentials};
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
-use crate::telemetry::spawn_blocking_with_tracing;
 use actix_web::body::BoxBody;
 use actix_web::http::header::{HeaderMap, HeaderValue};
 use actix_web::{http::StatusCode, web, HttpRequest, HttpResponse, ResponseError};
 use anyhow::{anyhow, Context};
-use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use reqwest::header;
-use secrecy::{ExposeSecret, Secret};
+use secrecy::Secret;
 use sqlx::PgPool;
 use std::fmt::Formatter;
 
@@ -77,7 +76,7 @@ pub async fn publish_newsletter(
         .map_err(PublishError::AuthError)?;
     tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
 
-    let user_id = validate_credentials(credentials, &pool)
+    let user_id = authentication::validate_credentials(credentials, &pool)
         .await
         // We match on `AuthError`'s variants, but we pass the **whole** error into the constructors
         // for `PublishError` variants. This ensures that the context of the top-level wrapper is
@@ -153,12 +152,6 @@ async fn get_confirmed_subscribers(
     Ok(confirmed_subscribers)
 }
 
-#[allow(dead_code)]
-struct Credentials {
-    username: String,
-    password: Secret<String>,
-}
-
 fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
     // The header value, if present must be a valid UTF8 string
     let header_value = headers
@@ -193,80 +186,4 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         username,
         password: Secret::new(password),
     })
-}
-
-#[tracing::instrument(name = "Validate Credentials", skip(credentials, pool))]
-async fn validate_credentials(
-    credentials: Credentials,
-    pool: &PgPool,
-) -> Result<uuid::Uuid, AuthError> {
-    let mut user_id = None;
-    let mut expected_password_hash = Secret::new(
-        "$argon2id$v=19$m=15000,t=2,p=1$\
-               gZiV/M1gPc22ElAH/Jh1Hw$\
-               CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
-            .to_string(),
-    );
-
-    if let Some((stored_user_id, stored_password_hash)) =
-        get_stored_credentials(&credentials.username, pool)
-            .await
-            .map_err(AuthError::UnexpectedError)?
-    {
-        user_id = Some(stored_user_id);
-        expected_password_hash = stored_password_hash;
-    }
-
-    spawn_blocking_with_tracing(move || {
-        verify_password_hash(expected_password_hash, credentials.password)
-    })
-    .await
-    .context("Failed to spawn blocking task.")
-    .map_err(AuthError::UnexpectedError)??;
-
-    // This is only set to `Some` if we found credentials in the store. So, even if the default
-    // password ends up matching (somehow) with the provided password, we never authenticate a non-existing
-    // user.
-    user_id.ok_or_else(|| AuthError::InvalidCredentials(anyhow!("Unknown username.")))
-}
-
-#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
-async fn get_stored_credentials(
-    username: &str,
-    pool: &PgPool,
-) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
-    let row = sqlx::query!(
-        r#"
-        SELECT user_id, password_hash
-        FROM users
-        WHERE username = $1
-        "#,
-        username
-    )
-    .fetch_optional(pool)
-    .await
-    .context("Failed to perform a query to retrieve stored credentials.")?
-    .map(|row| (row.user_id, Secret::new(row.password_hash)));
-
-    Ok(row)
-}
-
-#[tracing::instrument(
-    name = "Verify password hash",
-    skip(expected_password_hash, password_candidate)
-)]
-fn verify_password_hash(
-    expected_password_hash: Secret<String>,
-    password_candidate: Secret<String>,
-) -> Result<(), AuthError> {
-    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
-        .context("Failed to parse hash in PHC string format.")?;
-
-    Argon2::default()
-        .verify_password(
-            password_candidate.expose_secret().as_bytes(),
-            &expected_password_hash,
-        )
-        .context("Invalid password.")
-        .map_err(AuthError::InvalidCredentials)
 }
