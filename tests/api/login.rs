c331c6f644efa1c366a56464bec01a126cb9de09
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-15 08:46:00 +0100

    Add a simple test and code for setting and reading cookies

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,6 +1,12 @@
 use crate::helpers;
 use crate::helpers::spawn_app;
 
+/// Cookies are set by attaching a special HTTP header to the response-`Set-Cookie`. In its simplest
+/// form it looks like this:
+/// `Set-Cookie: {cookie-name}={cookie-value}`
+///
+/// `Set-Cookie` can be specified multiple times - one for each cookie you want to set. reqwest
+/// provides the `get-all` method to deal with multi-value headers:
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
     // Arrange
@@ -16,4 +22,8 @@ async fn an_error_flash_message_is_set_on_failure() {
     // Assert
     assert_eq!(response.status().as_u16(), 303);
     helpers::assert_is_redirect_to(&response, "/login");
+
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
 }
+
