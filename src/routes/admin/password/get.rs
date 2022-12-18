e68c34981518c9ba549b07d067cb649496069f19
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-18 09:29:00 +0100

    WIP for password reset workflow

--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -1,10 +1,17 @@
+use crate::session_state::TypedSession;
+use crate::utils::{e500, see_other};
 use actix_web::http::header::ContentType;
 use actix_web::{web, HttpResponse};
 use tera::{Context, Tera};
 
 pub async fn change_password_form(
     templates: web::Data<&Tera>,
+    session: TypedSession,
 ) -> Result<HttpResponse, actix_web::Error> {
+    if session.get_user_id().map_err(e500)?.is_none() {
+        return Ok(see_other("/login"));
+    }
+
     let context = Context::new();
     Ok(HttpResponse::Ok().content_type(ContentType::html()).body(
         templates
