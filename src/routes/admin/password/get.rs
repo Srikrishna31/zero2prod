fb0dd5026437cbc027ada0c6bab8bf056050573a
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-19 13:19:02 +0100

    Remove user validation code from admin/get endpoint

--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -1,5 +1,5 @@
-use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
+use crate::authentication::UserId;
 use actix_web::http::header::ContentType;
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::IncomingFlashMessages;
@@ -8,12 +8,10 @@ use tera::{Context, Tera};
 
 pub async fn change_password_form(
     templates: web::Data<&Tera>,
-    session: TypedSession,
+    user_id: web::ReqData<UserId>,
     flash_messages: IncomingFlashMessages,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
-        return Ok(see_other("/login"));
-    }
+    let _user_id = user_id.into_inner();
 
     let mut error_message = String::new();
     for m in flash_messages.iter() {
