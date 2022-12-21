c371b1b4c92f95a2a0007df53d088f857957ea22
Krishna Addepalli <coolkrishna31@gmail.com>
2022-12-21 09:48:18 +0100

    Initial implementation of idempotent newsletter delivery

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub mod authentication;
 pub mod configuration;
 pub mod domain;
 pub mod email_client;
+mod idempotency;
 pub mod routes;
 pub mod session_state;
 pub mod startup;
 pub mod telemetry;
 mod utils;
-mod idempotency;
 
 extern crate tera;
