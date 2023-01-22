c9cc6d6d882a1cbd7a06f67290a259c241560aa6
Krishna Addepalli <coolkrishna31@gmail.com>
2023-01-22 21:49:37 +0100

    Fixed the failing tests - mostly done

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -2,7 +2,7 @@ use super::IdempotencyKey;
 use actix_web::{body::to_bytes, http::StatusCode, HttpResponse};
 use anyhow::anyhow;
 use sqlx::postgres::PgTypeInfo;
-use sqlx::{postgres::PgHasArrayType, PgPool};
+use sqlx::{postgres::PgHasArrayType, PgPool, Transaction, Postgres};
 use uuid::Uuid;
 
 #[derive(Debug, sqlx::Type)]
@@ -67,7 +67,7 @@ pub async fn get_saved_response(
 /// * *Do whatever you have to do with the body;*
 /// * Re-assemble the response using .set_body() on the request head.
 pub async fn save_response(
-    pool: &PgPool,
+    mut transaction: Transaction<'static, Postgres>,
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
     http_response: HttpResponse,
@@ -104,7 +104,7 @@ pub async fn save_response(
         headers,
         body.as_ref()
     )
-    .execute(pool)
+    .execute(&mut transaction)
     .await?;
 
     // We need `.map_into_boxed_body` to go from `HttpResponse<Bytes>` to `HttpResponse<BoxBody>`
@@ -119,7 +119,8 @@ impl PgHasArrayType for HeaderPairRecord {
 }
 
 pub enum NextAction {
-    StartProcessing,
+    // Return transaction for later usage
+    StartProcessing(Transaction<'static, Postgres>),
     ReturnSavedResponse(HttpResponse),
 }
 
@@ -128,6 +129,7 @@ pub async fn try_processing(
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
 ) -> Result<NextAction, anyhow::Error> {
+    let mut transaction = pool.begin().await?;
     let n_inserted_rows = sqlx::query!(
         r#"
         INSERT INTO idempotency (
@@ -141,12 +143,12 @@ pub async fn try_processing(
         user_id,
         idempotency_key.as_ref()
     )
-    .execute(pool)
+    .execute(&mut transaction)
     .await?
     .rows_affected();
 
     if n_inserted_rows > 0 {
-        Ok(NextAction::StartProcessing)
+        Ok(NextAction::StartProcessing(transaction))
     } else {
         let saved_response = get_saved_response(pool, idempotency_key, user_id)
             .await?
