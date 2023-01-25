8742d2735609a0b0f250c7bb092e4d8a66e204ac
Krishna Addepalli <coolkrishna31@gmail.com>
2023-01-25 09:06:55 +0100

    Configure main to run the tasks in the background

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ pub mod configuration;
 pub mod domain;
 pub mod email_client;
 mod idempotency;
+pub mod issue_delivery_worker;
 pub mod routes;
 pub mod session_state;
 pub mod startup;
 pub mod telemetry;
 mod utils;
-mod issue_delivery_worker;
 
 extern crate tera;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use zero2prod::issue_delivery_worker::run_worker_until_stopped;
 use zero2prod::{configuration, startup::Application, telemetry};
 
 /// # tracing-subscriber
@@ -32,11 +33,17 @@ async fn main() -> anyhow::Result<()> {
     let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
 
-    let application = Application::build(configuration).await?;
+    let application = Application::build(configuration.clone()).await?;
+    let port = application.port();
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
 
-    println!("Running the server on: {address}: {}", application.port());
+    tokio::select! {
+        _ = application_task => {},
+        _ = worker_task => {},
+    };
 
-    application.run_until_stopped().await?;
+    println!("Running the server on: {address}:{port}");
 
     Ok(())
 }
